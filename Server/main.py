d58b6ab569e83897a00b3fd330cd859c7ea86d63
vic <2252262965@qq.com>
2024-06-19 18:09:00 +0800

    细节修改

--- Server/main.py
+++ Server/main.py
@@ -2,10 +2,10 @@ from flask import Flask, Response
 from flask_cors import CORS
 import json
 import time, os
-
+local_path=r'C:\Users\Y-vic\Desktop\大三\电创\前端\visiualization\Server'
 app = Flask(__name__)
 CORS(app)  # 添加这一行来启用 CORS
-data_source = "data.json"
+data_source = os.path.join(local_path, "data.json")
 
 
 @app.route('/')
@@ -33,5 +33,7 @@ def stream_data():
         time.sleep(1)
 
 
+
 if __name__ == '__main__':
     app.run(debug=True)
+
